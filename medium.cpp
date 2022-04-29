6a942ed01a4bdf227127938d1d9c8e79165adedb
Daniyal-Murtaza <86107799+Daniyal-Murtaza@users.noreply.github.com>
2022-04-29 09:00:34 +0500

    asteroid-point-zero

--- medium.cpp
+++ medium.cpp
@@ -4,79 +4,81 @@ Medium::Medium(int x) : Flying_Object({9, 321, 33, 61}, {x, 0, 50, 50}) {}
 
 void Medium::fly()
 {
+
+    // Sprite will flap it wings
     if (!exploded)
     {
         switch (frame)
         {
         case 0:
-            srcRect = {9, 321, 33, 61};
+            srcRect = {11, 422, 33, 61};
             frame = 1;
             break;
         case 1:
-            srcRect = {59, 322, 33, 61};
+            srcRect = {61, 423, 33, 61};
             frame = 2;
             break;
         case 2:
-            srcRect = {110, 326, 32, 56};
+            srcRect = {112, 427, 32, 56};
             frame = 3;
             break;
         case 3:
-            srcRect = {161, 327, 33, 51};
+            srcRect = {163, 428, 33, 51};
             frame = 4;
             break;
         case 4:
-            srcRect = {212, 328, 33, 48};
+            srcRect = {214, 429, 33, 48};
             frame = 5;
             break;
         case 5:
-            srcRect = {263, 328, 33, 48};
+            srcRect = {265, 429, 33, 48};
             frame = 6;
             break;
         case 6:
-            srcRect = {314, 328, 34, 48};
+            srcRect = {316, 429, 34, 48};
             frame = 7;
             break;
         case 7:
-            srcRect = {363, 327, 37, 49};
+            srcRect = {365, 428, 37, 49};
             frame = 8;
             break;
         case 8:
-            srcRect = {411, 326, 39, 51};
+            srcRect = {413, 427, 39, 51};
             frame = 0;
             break;
         }
-        moverRect.y += 15;
+        moverRect.y += 14;
     }
     else
     {
         switch (frame)
         {
         case 0:
-            srcRect = {9, 321, 33, 61};
+            srcRect = {41, 880, 92, 95};
             frame = 1;
             break;
         case 1:
-            srcRect = {40, 780, 92, 95};
+            srcRect = {191, 859, 133, 141};
             frame = 2;
             break;
         case 2:
-            srcRect = {190, 759, 133, 141};
+            srcRect = {341, 848, 162, 165};
             frame = 3;
             break;
         case 3:
-            srcRect = {340, 748, 162, 165};
+            srcRect = {508, 848, 162, 165};
             frame = 4;
             break;
         case 4:
-            srcRect = {507, 748, 162, 165};
+            srcRect = {683, 848, 154, 165};
             frame = 5;
             break;
         case 5:
-            srcRect = {682, 748, 154, 165};
+            srcRect = {849, 848, 154, 165};
             frame = 6;
             break;
         case 6:
-            srcRect = {848, 748, 154, 165};
+            srcRect = {1012, 848, 158, 165};
             // frame = 7;
             end = true;
             break;
